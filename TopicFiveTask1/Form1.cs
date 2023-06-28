441e42c06d242b651316242633aa8eb80697bb04
LoRD Ju$uCH <LoRD Ju$uCH@DESKTOP-4LI13R2>
2023-06-28 12:05:04 +0300

    Add project files.

---
+++ TopicFiveTask1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TopicFiveTask1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        StatisticalData number1 = new StatisticalData();
+        StatisticalData number2 = new StatisticalData();
+        StatisticalData number3 = new StatisticalData();
+        StatisticalData number4 = new StatisticalData();
+        StatisticalData number5 = new StatisticalData();
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int num1 = Int32.Parse(txtNum.Text);
+            int num2 = Int32.Parse(txtNum2.Text);
+            int num3 = Int32.Parse(txtNum3.Text);
+            
+            //number1.smallestNum(txtNum.Text)
+
+            number1.Num1 = num1;
+            number1.Num2 = num2;
+            number1.Num3 = num3;
+
+            number1.smallestNum();
+            number1.largestNum();
+            number1.average();
+        }
+    }
+}
---
+++ TopicFiveTask1/StatisticalData.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TopicFiveTask1
+{
+    internal class StatisticalData
+    {
+        private int num1;
+        private int num2; private int num3;
+
+        public int Num1
+        {
+            get { return num1; }
+            set { num1 = value; }
+        }
+
+        public int Num2
+        {
+            get { return num2; }
+            set { num2 = value; }
+        }
+        public int Num3
+        {
+            get { return num3; }
+            set { num3 = value; }
+        }
+
+        public void largestNum()
+        {
+            int num;
+            if (num1 > num2 && num1 > num3)
+            {
+                num = num1;
+                MessageBox.Show($"{num} is the largest number");
+            }
+            else if (num2 > num3 && num2 > num3)
+            {
+                num = num2;
+                MessageBox.Show($"{num} is the lasrgest number");
+            }
+            else if (num3 > num1 && num3 > num2)
+            {
+                num = num3; 
+                MessageBox.Show($"{num} is the largest number");
+            }
+        }
+
+        public void smallestNum()
+        {
+            int num;
+
+            if (num1 < num2 && (num1 < num3))
+            {
+                num = num1;
+                MessageBox.Show($"{num} is the smallest number");
+            }
+
+            else if (num2 < num3 && (num2 < num1))
+            {
+                num = num2;
+                MessageBox.Show($"{num} is the smallest number");
+            }
+            else if (num3 < num1 && (num3 < num2))
+            {
+                num = num3;
+                MessageBox.Show($"{num} is the smallest number");
+            }
+        }
+
+        public void average()
+        {
+            int num = (num1 + num2 + num3) / 3;
+
+            MessageBox.Show($"Average: {num}"); 
+        }
+    }
+}
